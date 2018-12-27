9565468e5e5fdc7225011609956564568ed325b7
Ash_zd <ash_zd@qq.com>
2018-12-27 09:59:46 +0800

    init final

---
+++ src/final project/src/main/java/javaweb/config/WebAppConfig.java
@@ -0,0 +1,19 @@
+package javaweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Ash
+ * @date 2018/12/20 9:23
+ */
+@Configuration
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
---
+++ src/final project/src/main/java/javaweb/controller/MainController.java
@@ -0,0 +1,12 @@
+package javaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Ash
+ * @date 2018/12/19 19:15
+ */
+@Controller
+public class MainController {
+}
